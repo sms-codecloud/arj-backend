b59e7aff11ecb0d491f0773890d9ebb45d4dbd37
shankarbk <sbk.qubits@gmail.com>
2025-10-01 15:41:54 +0530

    testing .zip with pipeline

--- lambda-hello-world/scripts/deploy_with_terraform.sh
+++ lambda-hello-world/scripts/deploy_with_terraform.sh
@@ -8,10 +8,10 @@ set -euo pipefail
 
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 ZIP_PATH="$ROOT_DIR/lambda.zip"
-TF_DIR="$ROOT_DIR/terraform"
+# TF_DIR="$ROOT_DIR/terraform"
 
 echo "Root: $ROOT_DIR"
-echo "Terraform dir: $TF_DIR"
+# echo "Terraform dir: $TF_DIR"
 echo "Zip path: $ZIP_PATH"
 
 if [ ! -f "$ZIP_PATH" ]; then
@@ -19,22 +19,22 @@ if [ ! -f "$ZIP_PATH" ]; then
   exit 2
 fi
 
-if ! command -v terraform >/dev/null 2>&1; then
-  echo "Error: terraform not found in PATH."
-  exit 3
-fi
+# if ! command -v terraform >/dev/null 2>&1; then
+#   echo "Error: terraform not found in PATH."
+#   exit 3
+# fi
 
 # Ensure AWS credentials are present (injected by Jenkins)
-: "${AWS_ACCESS_KEY_ID:?AWS_ACCESS_KEY_ID is required (inject from Jenkins credentials)}"
-: "${AWS_SECRET_ACCESS_KEY:?AWS_SECRET_ACCESS_KEY is required (inject from Jenkins credentials)}"
-AWS_REGION="${AWS_REGION:-ap-south-1}"
+# : "${AWS_ACCESS_KEY_ID:?AWS_ACCESS_KEY_ID is required (inject from Jenkins credentials)}"
+# : "${AWS_SECRET_ACCESS_KEY:?AWS_SECRET_ACCESS_KEY is required (inject from Jenkins credentials)}"
+# AWS_REGION="${AWS_REGION:-ap-south-1}"
 
-export AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN AWS_REGION
+# export AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN AWS_REGION
 
-echo "Using AWS region: $AWS_REGION"
-cd "$TF_DIR"
+# echo "Using AWS region: $AWS_REGION"
+# cd "$TF_DIR"
 
-terraform init -input=false
-terraform apply -auto-approve -var "lambda_zip=${ZIP_PATH}" -var "aws_region=${AWS_REGION}"
+# terraform init -input=false
+# terraform apply -auto-approve -var "lambda_zip=${ZIP_PATH}" -var "aws_region=${AWS_REGION}"
 
-echo "Terraform apply completed."
+# echo "Terraform apply completed."
